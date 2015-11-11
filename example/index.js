e8aacfe742356038a64ab2749d719b933ff9d4a2
Adrian <github@netzkind.org>
2015-11-11 08:44:43 +0100

    added persona sample
    - updated styles to 1.1.3

--- example/index.js
+++ example/index.js
@@ -1,9 +1,12 @@
 /// <reference path="../typings/angularjs/angular.d.ts"/>
-angular.module('testApp', ['fabric.ui.components', 'fabric.ui.components.datepicker',
+angular.module('testApp', [
+    'fabric.ui.components',
+    'fabric.ui.components.datepicker',
     'fabric.ui.components.toggle',
     'fabric.ui.components.textbox',
     'fabric.ui.components.dropdown',
-    'fabric.ui.components.choicefield'
+    'fabric.ui.components.choicefield',
+    'fabric.ui.components.persona'
 ]);
 var controller = angular.module('testApp').controller("testController", function ($scope) {
     $scope.toggled = false;
